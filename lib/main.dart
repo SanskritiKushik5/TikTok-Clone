d7270a21ce065e78dfcf069712236ff10ebd2526
SanskritiKushik5 <sanskritikushik23@gmail.com>
2023-06-18 18:32:53 +0530

    Auth Controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_app/constants.dart';
+import 'package:tiktok_app/controller/auth_controller.dart';
 import 'package:tiktok_app/view/screens/auth/signup_screen.dart';
-import 'package:tiktok_app/view/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -13,7 +19,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
         title: 'TikTok',
         debugShowCheckedModeBanner: false,
         theme: ThemeData.dark().copyWith(
